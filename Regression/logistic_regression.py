b9821ed6cf0021f8d06f1726f758765b04547b1d
Felix-Muasya <66355304+Felix-Muasya@users.noreply.github.com>
2022-05-28 18:02:48 +0300

    Update logistic_regression.py

--- Regression/logistic_regression.py
+++ Regression/logistic_regression.py
@@ -17,17 +17,16 @@ data1 = pd.DataFrame(data0)
 data1["Heart Disease"] = data1['Heart Disease'].map({'Presence': 1, 'Absence': 0})
 
 
-#print(data1.to_string())
+
 X = data1.drop(["Heart Disease"], axis=1)
 y = data1["Heart Disease"]
 
 # introduce a confusion matrix
-
-
 X_train, X_text, y_train, y_test = train_test_split(X, y, train_size=0.75, random_state=10)
 
 logm1 = sm.GLM(y_train, (sm.add_constant(X_train)), family=sm.families.Binomial())
-#print(logm1.fit().summary())
+print(logm1.fit().summary())
+
 x = data1[['Age', 'Sex', 'Chest pain type', 'BP',
           'Cholesterol', 'FBS over 120','EKG results',
           'Max HR', 'Exercise angina', 'ST depression',
@@ -42,11 +41,11 @@ print(vif_data)
 
 
 
-#plt.figure(figsize=(20, 10))
-#sns.heatmap(data1.columns, annot=True)
-#plt.show()
-#print(data1.corr().to_string())
+plt.figure(figsize=(20, 10))
+sns.heatmap(data1.columns, annot=True)
+plt.show()
+print(data1.corr().to_string())
 sys.stdout.close()
-# print(data1.to_string())
+
 
 
